80cc2230ae342ed7b65185231a42425737307746
AlexKamaev <grmizza@gmail.com>
2021-10-31 20:39:06 +0300

    react.Context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
+  const [todos, setTodos] = React.useState([                /*Первый элемент - state, второй - функция которая может менять state*/
+    {id: 1, completed: false, title: "Купить хлеб"},
+    {id: 2, completed: false, title: "Купить молоко"},
+    {id: 3, completed: false, title: "Купить масло"}
+  ])
+  
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {                                 /*Переопределяем массив. На каждой итеррации принимаем объект todo*/
+        if (todo.id===id) {                               /*Если id элемента массива равно id пришедшему на счетчик toggleTodo*/
+          todo.completed = !todo.completed    
+        }
+      return todo
+    })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))        /*на каждой итеррации получаем todo, сравниваем его id с входящим.*/
+  }                                                       /*Если id не совпадает - оставляем его в массиве*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value = {{removeTodo}}>             {/*первые скобки - js, вторые - объект*/}
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <TodoList todos={todos} onToggle={toggleTodo}/>
+      </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,54 @@
+import React, {useContext} from 'react'
+import PropTypes from 'prop-types'
+import { findByLabelText } from '@testing-library/dom'
+import Context from '../context'
+
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+
+}
+
+ function TodoItem({todo, index, onChange}) { /* В компонент TodoItem (TodoList.js) передаем функцию onChange */
+    
+    const {removeTodo} = useContext(Context)              /* На выходе получаем объект, совпадающий со значением value из Context.Provider в App.js. */
+                                                /* У этого объекта есть ключ removeTodo из которого мы получаем значение removeTodo */
+    const classes = []
+    if (todo.completed) {
+        classes.push('done')
+    }
+
+    return (
+        <li style={styles.li}>
+            <span className = {classes.join(' ')}>
+                <input 
+                type="checkbox" 
+                style={styles.input} 
+                checked = {todo.completed}
+                onChange={ ()=> onChange(todo.id) }/> {/* При событии onChange вызывается метод onChange, куда передается id нажатого todo*/}
+                <strong>{index+1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+            <button className = 'rm' onClick = {removeTodo(todo.id)}>&times;</button>
+        </li>
+    )
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem
